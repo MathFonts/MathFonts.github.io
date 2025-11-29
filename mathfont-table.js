36513c8279eaa87db40cc8362bb87d7786bf57cd
David Carlisle <d.p.carlisle@gmail.com>
2025-11-29 17:16:47 +0000

    delete page path cookies

--- mathfont-table.js
+++ mathfont-table.js
@@ -284,7 +284,8 @@ var createCookie = function(name, value) {
 }
 
 var deleteCookie = function(name) {
-    document.cookie = name + "=; Expires=Thu, 01 Jan 1970 00:00:01 GMT; SameSite=Lax";
+    document.cookie = name + "=; Expires=Thu, 01 Jan 1970 00:00:01 GMT; path=" + document.location.pathname +"; SameSite=Lax";
+//    document.cookie = name + "=; Expires=Thu, 01 Jan 1970 00:00:01 GMT; SameSite=Lax";
 }
 
 function getCookie(c_name) {
