dc45b3fd85ebd1a378799dddbdf27ae8289f4142
David Carlisle <d.p.carlisle@gmail.com>
2026-08-18 16:32:16 +0100

    pl46 again

--- font-selection.js
+++ font-selection.js
@@ -37,6 +37,7 @@ const mathfont_list = {
     // Informal
     "Pennstander": "Pennstander Math Regular",
     "NagwaTK": "Nagwa TK Math",
+    "PL46": "PL46 Math",
 };
 
 document.addEventListener("DOMContentLoaded", () => {
